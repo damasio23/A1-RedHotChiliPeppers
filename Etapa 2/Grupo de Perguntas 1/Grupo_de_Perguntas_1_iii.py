8690218e04a9de23233697866e27bd5f35eeb428
Anna Clara Damasio Monteiro <99307451+damasio23@users.noreply.github.com>
2022-10-02 13:58:58 -0300

    Etapa 2: Grupo de Perguntas 1: iii

---
+++ Etapa 2/Grupo de Perguntas 1/Grupo_de_Perguntas_1_iii.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+
+"""
+Músicas mais ouvidas e músicas menos ouvidas [em toda a história da banda ou artista]
+ 
+"""
+##Tabela com as informações. 
+df_excel = pd.read_excel("new_df.xls", index_col=[0,1])
+
+#Função para retornar a música mais ouvida em toda a história da banda.
+def musica_mais_ouvida(DataFrame):
+    ouvintes = DataFrame["Ouvintes"][DataFrame["Ouvintes"] == DataFrame["Ouvintes"].max()]
+    return ouvintes   
+print("Música mais ouvida em toda a história da banda\n", musica_mais_ouvida(df_excel))
+
+#Função para retornar a música menos ouvida em toda a história da banda.
+def musica_menos_ouvidas(DataFrame):
+    ouvintes = DataFrame["Ouvintes"][DataFrame["Ouvintes"] == DataFrame["Ouvintes"].min()]
+    return ouvintes   
+print("Música menoss ouvida em toda a história da banda\n", musica_menos_ouvidas(df_excel))
+
+
+
+
+
+
+
+
