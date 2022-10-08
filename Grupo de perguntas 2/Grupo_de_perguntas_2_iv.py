e6bed065db3a6d4ae04a0ec13b7ad8bd6cbf68cc
Luan-vht3 <105639491+Luan-vht3@users.noreply.github.com>
2022-10-08 12:03:27 -0300

    Trate exceções em Grupo_de_perguntas_2_iv.py

--- Grupo de perguntas 2/Grupo_de_perguntas_2_iv.py
+++ Grupo de perguntas 2/Grupo_de_perguntas_2_iv.py
@@ -9,27 +9,39 @@ termos_invalidos = ['the', 'a', 'an', 'it', 'some', 'any', 'to', 'in', 'on', 'at
 ###################################################################################################################################
 def palavras_mais_comuns_letras_discografia(dataframe):
     """Função que retorna as palavras mais comuns nas letras de música de toda a discografia da banda
-
+    Consideramos como mais comuns as palavras que tiverem frequência maior que o desvio padrão das frequências das palavras nas letras das músicas.
+     
     :param dataframe: Dataframe fonte
     :type dataframe: pandas.core.frame.DataFrame
-    :return: Série com as palavras mais comuns da discografia da banda como índices e suas respectivas frequências como valores  
+    :return: Série com as palavras mais comuns como índices e suas respectivas frequências como valores  
     :rtype: pandas.core.series.Series
     """
+    try:
+        dataframe_copia = dataframe.copy()
+        dataframe_copia.reset_index(inplace=True)
+    except AttributeError:
+        return "Erro na função palavras_mais_comuns_letras_discografia(): O tipo do argumento deve ser um dataframe de pandas."
     
-    dataframe_copia = dataframe.copy()
-    dataframe_copia.reset_index(inplace=True)
-
     # Vamos criar uma lista com as letras de todas as músicas da discografia.
-    letras_musicas = list(dataframe_copia['Letra'])
+    try:
+        letras_musicas = list(dataframe_copia['Letra'])
+    except KeyError:
+        return "Erro na função palavras_mais_comuns_letras_discografia(): No dataframe informado, não há nenhuma coluna com o nome 'Letra'."
 
     # Todas as palavras de todas as letras serão colocadas em uma única lista.
     letras_palavras_separadas = []
 
     # Vamos iterar sobre cada letra de música da banda, separando as palavras.
     for letra_musica in letras_musicas:
-        letra_musica.split() # Lista das palavras que compõem a letra da música em questão
-        for palavra in letra_musica.split():
-            letras_palavras_separadas.append(palavra.lower())
+        try:
+            if type(letra_musica) != str:
+                raise AttributeError
+        except AttributeError:
+            return "Erro na função palavras_mais_comuns_letras_discografia(): Os elementos da coluna Letra devem ser strings."
+        else:
+            letra_musica.split() # Lista das palavras que compõem a letra da música em questão
+            for palavra in letra_musica.split():
+                letras_palavras_separadas.append(palavra.lower())
     
     # Organizando a lista por ordem alfabética
     letras_palavras_separadas.sort()
@@ -52,7 +64,7 @@ def palavras_mais_comuns_letras_discografia(dataframe):
     frequencia_palavras_validas = pd.Series(serie_palavras_validas).value_counts()
     
     # A distribuição das frequências das palavras é bastante assimétrica à direita, isto é, grande parte das ocorrências está à esquerda da média
-    # Consideraremos, então, as palavras que tiverem frequência maior ou igual ao desvio padrão.
+    # Consideraremos, então, as palavras que tiverem frequência maior que o desvio padrão.
     desvio_frequencias = frequencia_palavras_validas.std()
 
     # Este é o dicionário em que vamos armazenar os registros mais frequentes, a serem tirados da série frequencia_palavras_validas
@@ -65,8 +77,8 @@ def palavras_mais_comuns_letras_discografia(dataframe):
         palavra = item_palavra_frequencia[0]
         frequencia = item_palavra_frequencia[1]
 
-        if frequencia >= desvio_frequencias:
-            # Ou seja, se a frequência da palavra em questão for maior ou igual ao desvio padrão das frequências, então ela entra no dicionário palavras_mais_frequentes
+        if frequencia > desvio_frequencias:
+            # Ou seja, se a frequência da palavra em questão for maior que o desvio padrão das frequências, então ela entra no dicionário palavras_mais_frequentes
             palavras_mais_frequentes[palavra] = frequencia
     
     serie_palavras_mais_frequentes = pd.Series(palavras_mais_frequentes)
