5a76a46603ac99227974e6f7d689df9b2eff73d8
Luan-vht3 <105639491+Luan-vht3@users.noreply.github.com>
2022-10-08 12:00:29 -0300

    Trate exceções em Grupo_de_perguntas_2_iii.py

--- Grupo de perguntas 2/Grupo_de_perguntas_2_iii.py
+++ Grupo de perguntas 2/Grupo_de_perguntas_2_iii.py
@@ -9,18 +9,26 @@ termos_invalidos = ['the', 'a', 'an', 'it', 'some', 'any', 'to', 'in', 'on', 'at
 ###################################################################################################################################
 def palavras_mais_comuns_letras_album(dataframe):
     """Função que retorna as palavras mais comuns nas letras das músicas por álbum da banda
-
+    Consideramos como mais comuns as palavras que tiverem frequência maior que o desvio padrão das frequências das palavras nas letras de música por álbum.
+        
     :param dataframe: Dataframe fonte
     :type dataframe: pandas.core.frame.DataFrame
-    :return: Série com os álbuns da banda como chaves, e como valores, o dicionário com as palavras como chaves e as frequências como valores  
+    :return: Série cujos índices são os álbuns da banda, e os valores são dicionários nos quais as palavras mais comuns são as chaves, e as frequências são os valores.
     :rtype: pandas.core.series.Series
     """
-    
-    dataframe_copia = dataframe.copy()
-    dataframe_copia.reset_index(inplace=True)
 
-    nomes_albuns = list(set(dataframe_copia['Album']))
-    nomes_albuns.sort()
+    try:
+        dataframe_copia = dataframe.copy()
+        dataframe_copia.reset_index(inplace=True)
+    except AttributeError:
+        return "Erro na função palavras_mais_comuns_letras_album(): O tipo do argumento deve ser um dataframe de pandas."
+    
+    # Lista com nomes dos álbums
+    try:
+        nomes_albuns = list(set(dataframe_copia['Album']))
+        nomes_albuns.sort()
+    except KeyError:
+        return "Erro na função palavras_mais_comuns_letras_album(): No dataframe informado não há nenhuma coluna com o nome 'Album'."
 
     # Esta função retornará um dicionário cujas chaves são o nome do álbum e os valores são outros dicionários
     # Cada um destes dicionários terá como chaves as palavras mais comuns de suas músicas, e como valor, a frequência desta palavra
@@ -29,18 +37,33 @@ def palavras_mais_comuns_letras_album(dataframe):
     # Vamos iterar sobre cada álbum da banda, que corresponderão a cada item do dicionário
     for nome_album in nomes_albuns:
         # Acessando o sub-dataframe das músicas do álbum em questão
-        df_musicas_album = dataframe.loc[nome_album]
-        df_musicas_album.reset_index(inplace=True)
+        try:
+            df_musicas_album = dataframe.loc[nome_album]
+            df_musicas_album.reset_index(inplace=True)
+        except KeyError:
+            return "Erro na função palavras_mais_comuns_letras_album(): O dataframe informado não possui multiindex."
 
         # Definimos uma lista que vai conter todas as palavras de todas as letras de músicas do álbum em questão
         letras_palavras_separadas = []
 
+        # Lista das letras das músicas
+        try:
+            letras_musicas = list(df_musicas_album["Letra"])
+        except KeyError:
+            return "Erro na função palavras_mais_comuns_letras_album(): No dataframe informado não há nenhuma coluna com o nome 'Letra'."
+
         # Pegando a letra de cada música. Aqui, letra_musica é uma longa string com a letra de cada música
-        for letra_musica in list(df_musicas_album["Letra"]):
-            letra_musica.split()
-            for palavra in letra_musica.split():
-                letras_palavras_separadas.append(palavra.lower())
-        
+        for letra_musica in letras_musicas:
+            try:
+                if type(letra_musica) != str:
+                    raise AttributeError
+            except AttributeError:
+                return "Erro na função palavras_mais_comuns_letras_album(): Os elementos da coluna Letra devem ser strings."
+            else:
+                letra_musica.split()
+                for palavra in letra_musica.split():
+                    letras_palavras_separadas.append(palavra.lower())
+    
         # Organizando a lista por ordem alfabética
         letras_palavras_separadas.sort()
 
@@ -62,7 +85,7 @@ def palavras_mais_comuns_letras_album(dataframe):
         frequencia_palavras_validas = pd.Series(serie_palavras_validas).value_counts()
         
         # A distribuição das frequências das palavras é bastante assimétrica à direita, isto é, grande parte das ocorrências está à esquerda da média
-        # Consideraremos, então, as palavras que tiverem frequência maior ou igual ao desvio padrão.
+        # Consideraremos, então, as palavras que tiverem frequência maior que o desvio padrão.
         desvio_frequencias = frequencia_palavras_validas.std()
 
         # Este é o dicionário em que vamos armazenar os registros mais frequentes, a serem tirados da série frequencia_palavras_validas
@@ -75,8 +98,8 @@ def palavras_mais_comuns_letras_album(dataframe):
             palavra = item_palavra_frequencia[0]
             frequencia = item_palavra_frequencia[1]
 
-            if frequencia >= desvio_frequencias:
-                # Ou seja, se a frequência da palavra em questão for maior ou igual ao desvio padrão das frequências, então ela entra no dicionário palavras_mais_frequentes
+            if frequencia > desvio_frequencias:
+                # Ou seja, se a frequência da palavra em questão for maior que o desvio padrão das frequências, então ela entra no dicionário palavras_mais_frequentes
                 palavras_mais_frequentes[palavra] = frequencia
 
         # Finalmente, vamos criar o item do dicionário dict_palavras_mais_frequentes, passando como chave o nome do álbum, e como valor, o dicionário palavras_mais_frequentes
